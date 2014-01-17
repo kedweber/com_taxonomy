5b3ce5562a8ed56a1ad7001c5e90ae7f648f08a2
Dave Li <dave.li@imagegroup.nl>
2014-01-17 11:45:51 +0100

    Added missing frontend files and added custom getTotal() for better support.

--- components/com_taxonomy/models/default.php
+++ components/com_taxonomy/models/default.php
@@ -64,4 +64,31 @@ class ComTaxonomyModelDefault extends ComDefaultModelDefault
             $query->having($having);
         }
     }
+
+    /**
+     * Specialized to NOT use a count query since all the inner joins get confused over it
+     *
+     * @see KModelTable::getTotal()
+     */
+    public function getTotal()
+    {
+        // Get the data if it doesn't already exist
+        if (!isset($this->_total)) {
+            if ($this->isConnected()) {
+                $query = $this->getTable()->getDatabase()->getQuery();
+
+                $this->_buildQueryColumns($query);
+                $this->_buildQueryFrom($query);
+                $this->_buildQueryJoins($query);
+                $this->_buildQueryWhere($query);
+                $this->_buildQueryGroup($query);
+                $this->_buildQueryHaving($query);
+
+                $total = count($this->getTable()->select($query, KDatabase::FETCH_FIELD_LIST));
+                $this->_total = $total;
+            }
+        }
+
+        return $this->_total;
+    }
 }
