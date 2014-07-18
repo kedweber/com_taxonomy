6c733d34aa120e53d2eee4fb181be9adf6147414
Dave Li <dave@moyoweb.nl>
2014-07-18 21:02:11 +0200

    Improved relations.

--- packages/component/components/com_taxonomy/databases/rows/default.php
+++ packages/component/components/com_taxonomy/databases/rows/default.php
@@ -12,6 +12,13 @@ class ComTaxonomyDatabaseRowDefault extends KDatabaseRowDefault
 
 		$taxonomies = json_decode($this->{$type});
 
+		if($this->getRelations()->{$type}->{$column}->fallback == 1) {
+			$identity_column = '';
+			$id = '';
+		} else {
+
+		}
+
 		if(KInflector::isSingular($column)) {
 			return $this->getService($relations->{$type}->{$column}->identifier)->id($taxonomies->{$column})->getItem();
 		}
@@ -36,41 +43,41 @@ class ComTaxonomyDatabaseRowDefault extends KDatabaseRowDefault
 	{
 		$result = parent::__get($column);
 
-		if(!empty($this->ancestors) && empty($result)) {
-			$ancestors = json_decode($this->ancestors, true);
+		if($this->isRelationable()) {
+			if(!empty($this->ancestors) && empty($result)) {
+				$ancestors = json_decode($this->ancestors, true);
 
-			if(is_array($ancestors) && array_key_exists($column, $ancestors)) {
-				$result = $this->__getRelation('ancestors', $column);
+				if(is_array($ancestors) && array_key_exists($column, $ancestors)) {
+					$result = $this->__getRelation('ancestors', $column);
 
-				$this->setData(array(
-					$column => $result
-				));
+					$this->setData(array(
+						$column => $result
+					));
 
-				return $result;
+					return $result;
+				}
 			}
-		}
 
-		if(!empty($this->descendants) && empty($result)) {
-			$descendants = json_decode($this->descendants, true);
+			if(!empty($this->descendants) && empty($result)) {
+				$descendants = json_decode($this->descendants, true);
 
-			if(is_array($descendants) && array_key_exists($column, $descendants)) {
-				$result = $this->__getRelation('descendants', $column);
+				if((is_array($descendants) && array_key_exists($column, $descendants)) || $this->getRelations()->descendants->{$column}->fallback == 1) {
+					$result = $this->__getRelation('descendants', $column);
 
-				$this->setData(array(
-					$column => $result
-				));
+					$this->setData(array(
+						$column => $result
+					));
 
-				return $result;
+					return $result;
+				}
 			}
-		}
 
-		return $result;
+			return $result;
+		}
 	}
 
 	public function getRelations()
 	{
-		if($this->isRelationable()) {
-			return $this->getTable()->getBehavior('relationable')->getRelations();
-		}
+		return $this->getTable()->getBehavior('relationable')->getRelations();
 	}
 }
