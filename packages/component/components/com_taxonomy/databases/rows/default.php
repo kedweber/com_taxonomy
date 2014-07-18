577cb3ba7bbf5f49879746ea5d6c830dd858e6db
Dave Li <dave@moyoweb.nl>
2014-07-18 21:56:49 +0200

    Fixed issue with regression.

--- packages/component/components/com_taxonomy/databases/rows/default.php
+++ packages/component/components/com_taxonomy/databases/rows/default.php
@@ -8,23 +8,26 @@ class ComTaxonomyDatabaseRowDefault extends KDatabaseRowDefault
 	 */
 	private function __getRelation($type, $column)
 	{
-		$relations = $this->getRelations();
-
+		$relations	= $this->getRelations();
 		$taxonomies = json_decode($this->{$type});
+		$identifier = new KServiceIdentifier($relations->{$type}->{$column}->identifier);
 
 		if($this->getRelations()->{$type}->{$column}->fallback == 1) {
-			$identity_column = '';
-			$id = '';
+			$identity_column = KInflector::singularize($this->getIdentifier()->name).'_id';
+			$id = $this->id;
 		} else {
-
+			$identity_column = 'id';
+			$id = $taxonomies->{$column};
 		}
 
 		if(KInflector::isSingular($column)) {
-			return $this->getService($relations->{$type}->{$column}->identifier)->id($taxonomies->{$column})->getItem();
-		}
+			$result = $this->getService($identifier)->set($identity_column, $id)->getItem();
 
-		if(KInflector::isPlural($column)) {
-			$model = $this->getService($relations->{$type}->{$column}->identifier);
+			if(!$result->id) {
+				$result = null;
+			}
+		} else {
+			$model = $this->getService($identifier);
 			$state = $model->getState();
 
 			if($relations->{$type}->{$column}->{$column}->state) {
@@ -35,49 +38,59 @@ class ComTaxonomyDatabaseRowDefault extends KDatabaseRowDefault
 				}
 			}
 
-			return $model->id($taxonomies->{$column})->getList();
+			try {
+				$result = $model->{$identity_column}($id)->getList();
+			} catch (Exception $e) {
+				return $result = null;
+			}
+
+			if($result->count() == 0) {
+				$result = null;
+			}
 		}
+
+		return $result;
 	}
 
 	public function __get($column)
 	{
 		$result = parent::__get($column);
 
-		if($this->isRelationable()) {
-			if(!empty($this->ancestors) && empty($result)) {
-				$ancestors = json_decode($this->ancestors, true);
+		if(!empty($this->ancestors) && empty($result)) {
+			$ancestors = json_decode($this->ancestors, true);
 
-				if(is_array($ancestors) && array_key_exists($column, $ancestors)) {
-					$result = $this->__getRelation('ancestors', $column);
+			if(is_array($ancestors) && array_key_exists($column, $ancestors)) {
+				$result = $this->__getRelation('ancestors', $column);
 
-					$this->setData(array(
-						$column => $result
-					));
+				$this->setData(array(
+					$column => $result
+				));
 
-					return $result;
-				}
+				return $result;
 			}
-
-			if(!empty($this->descendants) && empty($result)) {
-				$descendants = json_decode($this->descendants, true);
-
-				if((is_array($descendants) && array_key_exists($column, $descendants)) || $this->getRelations()->descendants->{$column}->fallback == 1) {
-					$result = $this->__getRelation('descendants', $column);
-
-					$this->setData(array(
-						$column => $result
-					));
-
-					return $result;
-				}
-			}
-
-			return $result;
 		}
+
+		if(!empty($this->descendants) && empty($result)) {
+			$descendants = json_decode($this->descendants, true);
+
+			if((is_array($descendants) && array_key_exists($column, $descendants)) || $this->getRelations()->descendants->{$column}->fallback == 1) {
+				$result = $this->__getRelation('descendants', $column);
+
+				$this->setData(array(
+					$column => $result
+				));
+
+				return $result;
+			}
+		}
+
+		return $result;
 	}
 
 	public function getRelations()
 	{
-		return $this->getTable()->getBehavior('relationable')->getRelations();
+		if($this->isRelationable()) {
+			return $this->getTable()->getBehavior('relationable')->getRelations();
+		}
 	}
 }
